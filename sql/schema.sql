e8d6634b1e346f27beb027387c10d88af804f876
Fujiwara-gumi <isucon@fujiwaragumi>
2014-09-28 11:00:23 +0900

    INDEXはる

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `login` varchar(255) NOT NULL UNIQUE,
   `password_hash` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL
+  `salt` varchar(255) NOT NULL,
+  INDEX `users_login` (`login`)
 ) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `login_log` (
@@ -11,5 +12,7 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL
+  `succeeded` tinyint NOT NULL,
+  INDEX `login_log_user_id_succeeded` (`user_id`, `succeeded`),
+  INDEX `login_log_ip_succeeded` (`ip`, `succeeded`)
 ) DEFAULT CHARSET=utf8;
